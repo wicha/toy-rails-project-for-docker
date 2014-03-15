2c5c2c79fde99c4c097fdaa476bced56208c93ee
Weiyan Shao <lighteningman@gmail.com>
2014-03-15 16:06:43 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ToyRailsProjectForDocker::Application.config.secret_key_base = '0ab2ebfb6976d733644e5529d0a70c6a888489cb2a09088f2248d1a8e16bf521af254b9757d2feb0ceac363e09df218195a863881b83d891ed003b2fb60283c8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ToyRailsProjectForDocker::Application.config.session_store :cookie_store, key: '_toy-rails-project-for-docker_session'
